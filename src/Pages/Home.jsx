45d256dfc67e4e1ff995fe7813f24a1ad509783f
mananjot-17 <mananjot.singh@aimlarger.com>
2025-02-01 17:21:50 +0530

    changed UI of home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -76,13 +76,15 @@ const WeatherApp = () => {
             className="flex flex-col items-center bg-white bg-opacity-20 p-2 md:p-4 rounded-lg shadow-lg w-16 md:w-24"
             whileHover={{ scale: 1.1 }}
           >
-            <p className="font-bold text-xs md:text-base">{day.day}</p>
+            <p className="font-bold text-gray-500 text-xs md:text-base">
+              {day.day}
+            </p>
             {day.condition === "Sunny" ? (
               <FaSun className="text-yellow-400 text-xl md:text-2xl" />
             ) : (
               <FaCloudRain className="text-blue-400 text-xl md:text-2xl" />
             )}
-            <p className="text-sm md:text-lg font-semibold">
+            <p className="text-sm text-gray-500 md:text-lg font-semibold">
               {convertTemp(day.temp)}°{unit}
             </p>
           </motion.div>
